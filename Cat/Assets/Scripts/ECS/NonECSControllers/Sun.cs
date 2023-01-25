4f176d813c3c5c6a4c3ec3c5c36cdecd300905f9
pekarnik <pekgoglo@gmail.com>
2023-01-25 19:38:42 +0300

    Add non ECS sun control

---
+++ Cat/Assets/Scripts/ECS/NonECSControllers/Sun.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+namespace NonECS
+{
+
+    public enum DayState
+    {
+        NONE = 0,
+        DAY = 1,
+        NIGHT = 2
+    }
+    public delegate DayState ChangeDay();
+    public class Sun:MonoBehaviour
+    {
+        event ChangeDay _changeDay;
+        [SerializeField]
+        private float _dayLength = 5.0f * 60;
+        [SerializeField]
+        private float _rotateToNight;
+        [SerializeField]
+        private float _rotateToDay;
+        [SerializeField]
+        private float _nightLength = 5.0f * 60;
+        private float _curTime;
+        DayState _dayState;
+        private float _startRot;
+        private void Start()
+        {
+            RunDay(DayState.DAY);
+            _curTime = _dayLength;
+        }
+        private void Update()
+        {
+            if (_curTime <= 0.0f)
+            {
+                _changeDay?.Invoke();
+                _dayState = DayState.NONE;
+            }
+            if(_dayState == DayState.DAY || _dayState == DayState.NIGHT)
+            {
+                _curTime -= Time.deltaTime;
+                RotateSun();
+            }
+        }
+        private void RunDay(DayState day)
+        {
+            _dayState = day;
+            _startRot = transform.rotation.eulerAngles.x;
+        }
+        private void RotateSun()
+        {
+            float lerp = 0;
+            float timeRot = 0;
+            if (_dayState == DayState.DAY)
+            {
+                timeRot = (_dayLength - _curTime) / _dayLength;
+                lerp = Mathf.Lerp(_startRot, _rotateToNight, timeRot);
+            }
+            else if(_dayState == DayState.NIGHT)
+            {
+                timeRot = (_nightLength - _curTime) / _nightLength;
+                lerp = Mathf.Lerp(_startRot, _rotateToNight, timeRot);
+            }
+            transform.rotation = Quaternion.Euler(lerp, -30, 0);
+        }
+    }
+}
---
+++ Cat/Assets/Scripts/ECS/Systems/InitLight.cs
@@ -0,0 +1,17 @@
+﻿using Client;
+using Components;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Systems
+{
+    sealed class InitLight : IEcsInitSystem
+    {
+        public void Init(EcsSystems systems)
+        {
+            EcsWorld world = systems.GetWorld();
+            var lightEntity = world.NewEntity();
+
+        }
+    }
+}
